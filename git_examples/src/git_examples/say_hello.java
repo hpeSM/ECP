3e0355f65f253c1e58ecfbfb5ac8845576184860
nairsuni <nairsuni@NAIRSUNI1.asiapacific.hpqcorp.net>
2016-07-20 12:09:00 +0530

    first ecp commit

---
+++ git_examples/src/git_examples/say_hello.java
@@ -0,0 +1,7 @@
+public class say_hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		System.out.println("this is shafeeque");
+	}
+} 
